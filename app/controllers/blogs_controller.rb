d31632c69d78094b341afbac94784c8d84592833
1412350 <ngoc120742@gmail.com>
2017-10-03 15:51:35 +0700

    Add Blog Index, Show, Create action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,19 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.where(user_id: params[:user_id])
+  end
+  def show
+    @blog = Blog.find(params[:id])
+  end
+  def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.new(tittle: params[:blog][:tittle], imgcover: params[:blog][:imgcover], content: params[:blog][:content], user: current_user, view: 0)
+    if @blog.save 
+      redirect_to user_blogs_url(current_user), notice: "Add successfully!"
+    else
+      redirect_to new_blog_url, alert: @blog.errors.full_messages[0]
+    end
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  mount_uploader :imgcover, BlogUploader
+  validates :tittle, presence: true
+  validates :content, presence: true
+  validates :imgcover, presence: true
+ 
 end

ba4bf54e18f7f055372952f8963afd0dc0fd957f
1412350 <ngoc120742@gmail.com>
2017-10-08 23:30:04 +0700

    fix Add album/images bugs

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -12,19 +12,19 @@ class AlbumsController < ApplicationController
     @image = Image.new
   end
   def create
-    @image = Image.new(url: params[:album][:image][:url], user: current_user, view: 0)
-    @album = Album.new(name: params[:album][:name], totalview: 0, user: current_user, imgcover: @image.url.url)
-    if @album.save
-      @image.album = @album
-      if @image.save
-        @album.update(imgcover: @image.url.thumb.url)
-        redirect_to user_albums_url(current_user), notice: "Add album successfully!"
+      @image = Image.new(url: params[:album][:image][:url], user: current_user, view: 0)
+      @album = Album.new(name: params[:album][:name], totalview: 0, user: current_user, imgcover: @image.url.url)
+      if @album.save
+        @image.album = @album
+        if @image.save
+          @album.update(imgcover: @image.url.thumb.url)
+          redirect_to user_albums_url(current_user), notice: "Add album successfully!"
+        else
+          redirect_to new_user_album_image_url(user: current_user, album: @album), alert: @image.errors.full_messages[0]
+        end        
       else
-        redirect_to new_user_album_image_url(user: current_user, album: @album), alert: @image.errors.full_messages[0]
-      end        
-    else
-      redirect_to new_user_album_url(user: current_user), alert: @album.errors.full_messages[0]
-    end     
+        redirect_to new_user_album_url(user: current_user), alert: @album.errors.full_messages[0]
+      end 
   end
   def destroy  
     @album = Album.find(params[:id])

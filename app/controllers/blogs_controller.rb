89101ddd3db3e3f77415dc5fcf09794f521ba621
1412350 <ngoc120742@gmail.com>
2017-10-09 15:03:07 +0700

    Fix blog action bugs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :has_permission_to_do_action, only: [:edit, :update, :destroy]
+  respond_to :html, :js
   def index
     @comment = Comment.new
     @blogs = Blog.all
@@ -29,13 +30,13 @@ class BlogsController < ApplicationController
     imgcover = params[:blog][:imgcover]
     if imgcover
       if @blog.update(tittle: tittle, content: content, imgcover: imgcover)
-        redirect_to user_blogs_url(current_user), notice: "Updated blog!"
+        redirect_to user_blog_url(current_user, @blog), notice: "Updated blog!"
       else
         redirect_to edit_user_blog_url(current_user, @blog), alert: @blog.errors.full_messages[0]
       end
     else
       if @blog.update(tittle: tittle, content: content)
-        redirect_to user_blogs_url(current_user), notice: "Updated blog!"
+        redirect_to user_blog_url(current_user, @blog), notice: "Updated blog!"
       else
         redirect_to edit_user_blog_url(current_user, @blog), alert: @blog.errors.full_messages[0]
       end
